e5a54cbcdac1bea1334840d00b6a0f0884f608bf
Subodh20 <subodhtiwari360@gmail.com>
2022-04-06 15:13:42 +1000

    first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from './components/store/AuthContext';
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(()=>{
+    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+    if(storedUserLoggedInInformation === '1'){
+      setIsLoggedIn(true);
+    }
+  },[])
+  
+  
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    setIsLoggedIn(true);
+    localStorage.setItem('isLoggedIn','1');
+  };
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+    localStorage.removeItem('isLoggedIn');
+  };
+
+  return (
+      <AuthContext.Provider value={{isLoggedIn:isLoggedIn}}>
+      <MainHeader  onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+      </AuthContext.Provider>
+  );
+}
+
+
+export default App;
